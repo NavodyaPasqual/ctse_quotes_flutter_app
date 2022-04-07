cf2265b325ca096ec82e46ef706926127d5062c5
tandin2000 <it19098838@my.sliit.lk>
2022-04-07 10:02:09 +0530

    new changes

--- lib/pages/main/viewer_dashboard_page.dart
+++ lib/pages/main/viewer_dashboard_page.dart
@@ -1,6 +1,7 @@
-import 'package:ctse_quotes_flutter_app/pages/userQuotes/user_quotes_list_page.dart';
 import 'package:flutter/material.dart';
 import 'package:ctse_quotes_flutter_app/pages/sidebarlib/NavBar.dart';
+import 'package:ctse_quotes_flutter_app/pages/viewer/view_quote.dart';
+
 class ViewerDashboardPage extends StatefulWidget {
   const ViewerDashboardPage({Key? key}) : super(key: key);
 
@@ -14,10 +15,16 @@ class _ViewerDashboardPageState extends State<ViewerDashboardPage> {
     return Scaffold(
         drawer: NavBar(),
         appBar: AppBar(
-          title: Text('SideBar'),
+          title: Text('Category'),
         ),
         body: Column(
-
+          children: <Widget>[
+            const SizedBox(height: 55),
+            FlatButton(onPressed: (){ Navigator.push(context, MaterialPageRoute(builder: (context) => const UserQuotesList()));},
+              color: Colors.redAccent,
+              child: const Text("User Quotes"),
+            ),
+          ]
         )
     );
   }
---
+++ lib/pages/viewer/view_quote.dart
@@ -0,0 +1,162 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ctse_quotes_flutter_app/pages/userQuotes/user_quotes_edit_page.dart';
+import 'package:ctse_quotes_flutter_app/pages/userQuotes/user_quotes_read_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class UserQuotesList extends StatefulWidget {
+  const UserQuotesList({Key? key}) : super(key: key);
+
+  @override
+  _UserQuotesListState createState() => _UserQuotesListState();
+}
+
+class _UserQuotesListState extends State<UserQuotesList> {
+  final Stream<QuerySnapshot> usersStream = FirebaseFirestore.instance
+      .collection('userQuotes').snapshots();
+
+  final TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.init(
+        BoxConstraints(
+            maxWidth: MediaQuery
+                .of(context)
+                .size
+                .width,
+            maxHeight: MediaQuery
+                .of(context)
+                .size
+                .height),
+        context: context);
+    return Scaffold(
+
+      backgroundColor: const Color(0xFF000000),
+
+      appBar: AppBar(
+        title: const Text("Quotes"),
+        titleTextStyle: const TextStyle(
+          color: Color(0xFFFCDAB7),
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
+        backgroundColor: const Color(0xFF000000),
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Color(0xFFFCDAB7)),
+          onPressed: () =>
+              Navigator.of(context).pop(),
+        ),
+      ),
+      body: StreamBuilder(
+        stream: usersStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Something is wrong");
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return Column(
+            children: <Widget>[
+              Expanded(
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(6),
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  //shrinkWrap: true,
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (_, index) {
+                    return Container(
+                        margin: const EdgeInsets.only(left: 4.0,top: 6.0,right: 4.0,bottom: 6.0),
+                        child: GestureDetector(
+                          onTap: () =>
+                              Navigator.push(context,PageRouteBuilder(
+                                  transitionDuration:const Duration(milliseconds: 600),
+                                  reverseTransitionDuration: const Duration(milliseconds: 420),
+                                  transitionsBuilder:(BuildContext context, Animation<double> animation,
+                                      Animation<double> secondaryAnimation, Widget child){
+                                    return ScaleTransition(
+                                      scale: animation,
+                                      child: child,
+                                    );
+                                  },
+
+                                  pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation){
+                                    return UserQuotesRead(docid: snapshot.data!.docs[index]);
+                                  }
+                              )
+                              ),
+                          child: Column(
+                            children: [
+                              Card(
+                                color: Colors.black,
+                                child: Container(
+                                  decoration: const BoxDecoration(
+                                    gradient: LinearGradient(
+                                      colors: [Color(0xFFFCDAB7), Color(0xFF313130)],
+                                      begin: Alignment.topLeft,
+                                      end: Alignment.bottomRight,
+                                    ),
+                                    borderRadius: BorderRadius.horizontal(
+                                        right: Radius.circular(20),
+                                        left: Radius.circular(20)),
+                                  ),
+                                  child: ListTile(
+                                    title: Text(
+                                      snapshot.data!.docChanges[index].doc['quote'],
+                                      style: GoogleFonts.josefinSans(
+                                        color: Colors.black,
+                                        fontStyle: FontStyle.italic,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      snapshot.data!.docChanges[index].doc['author'],
+                                      style: GoogleFonts.dosis(
+                                        color: Colors.black,
+                                        fontStyle: FontStyle.italic,
+                                      ),
+                                    ),
+                                    contentPadding: const EdgeInsets.only(
+                                        left: 22.0, right: 0.0,top: 12.0, bottom: 12.0
+                                    ),
+                                    trailing: IconButton(
+                                      onPressed: () {
+                                        Navigator.push(context, MaterialPageRoute(
+                                            builder: (_) =>
+                                                UserQuotesEdit(docid: snapshot.data!
+                                                    .docs[index]
+                                                )
+                                        )
+                                        );
+                                      },
+                                      icon: const Icon(
+                                        Icons.favorite,
+                                        size: 25,
+                                        color: Color(0xFFFCDAB7),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+
+}
