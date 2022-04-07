be3ba7f4c5420910d766b3a2057d4594f8650949
tandin2000 <it19098838@my.sliit.lk>
2022-04-07 09:37:19 +0530

    tan chnages

--- lib/pages/main/viewer_dashboard_page.dart
+++ lib/pages/main/viewer_dashboard_page.dart
@@ -1,6 +1,6 @@
 import 'package:ctse_quotes_flutter_app/pages/userQuotes/user_quotes_list_page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:ctse_quotes_flutter_app/pages/sidebarlib/NavBar.dart';
 class ViewerDashboardPage extends StatefulWidget {
   const ViewerDashboardPage({Key? key}) : super(key: key);
 
@@ -12,14 +12,12 @@ class _ViewerDashboardPageState extends State<ViewerDashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: NavBar(),
+        appBar: AppBar(
+          title: Text('SideBar'),
+        ),
         body: Column(
-          children: <Widget>[
-            const SizedBox(height: 55),
-            FlatButton(onPressed: (){ Navigator.push(context, MaterialPageRoute(builder: (context) => const UserQuotesList()));},
-              color: Colors.redAccent,
-              child: const Text("User Quotes"),
-            ),
-          ]
+
         )
     );
   }
